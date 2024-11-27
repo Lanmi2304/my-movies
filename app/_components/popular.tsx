f1daa34a5447ec5ca3b84d303d04d3fbc75724c8
Milan <milanpavlovic413@gmail.com>
2024-11-27 14:36:40 +0100

    feat: force-cache popular

--- app/_components/popular.tsx
+++ app/_components/popular.tsx
@@ -10,6 +10,7 @@ export async function Popular() {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'`,
     {
+      cache: "force-cache",
       headers: {
         accept: "application/json",
         Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
